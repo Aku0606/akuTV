e8e16806feb572a3ca499b784fd3053fa74565ed
Aku0606 <akulucky0606@gmail.com>
2025-05-11 16:07:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -227,5 +227,5 @@ const CUSTOM_API_CONFIG = {
     adultPropName: 'isAdult' // 用于标记成人内容的属性名
 };
 
-// 新增隐藏内置黄色采集站API的变量，默认为true
+// 新增隐藏内置黄色采集站API的变量，默认为falseconst HIDE_BUILTN_ADULT_APIS=false
 const HIDE_BUILTIN_ADULT_APIS = true;
